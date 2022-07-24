d7667bd5cdb2f10c30751d461b6749bc265f4f37
jaewoogwak <jaewoo010207@gmail.com>
2022-07-24 18:10:29 +0900

    refactor : 코드 리팩토링

--- src/features/date/dateSlice.js
+++ src/features/date/dateSlice.js
@@ -7,6 +7,7 @@ const dateSlice = createSlice({
     page: {
       year: new Date().getFullYear(),
       month: new Date().getMonth() + 1,
+      date: new Date().getDate(),
     },
   },
   reducers: {
