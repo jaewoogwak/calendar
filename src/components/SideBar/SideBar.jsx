f6f4acdd998d10826ccae641bb771108cf82ec4c
jaewoogwak <jaewoo010207@gmail.com>
2022-08-06 02:57:56 +0900

    feat : 모달 기능 삭제

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import { setDate } from "../../data/slices/dateSlice";
-import { setModalVisible } from "../../data/slices/modalSlice";
 import { addTodo, deleteTodo } from "../../data/slices/todoSlice";
 import Todo from "../DateList/Todo";
 
